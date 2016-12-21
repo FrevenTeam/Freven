fb68daa2ada57a9b3051f70e39447becb88d23a0
Apple <ytapplegamer@gmail.com>
2016-12-21 18:38:54 -0500

    Add AI back

--- src/pocketmine/Server.php
+++ src/pocketmine/Server.php
@@ -86,6 +86,7 @@ use pocketmine\entity\Wolf;
 use pocketmine\entity\XPOrb;
 use pocketmine\entity\Zombie;
 use pocketmine\entity\ZombieVillager;
+use pocketmine\entity\ai\AIHolder;
 use pocketmine\event\HandlerList;
 use pocketmine\event\level\LevelInitEvent;
 use pocketmine\event\level\LevelLoadEvent;
@@ -354,6 +355,9 @@ class Server{
 	public $weatherRandomDurationMax = 12000;
 	public $lightningTime = 200;
 	public $lightningFire = false;
+	public $aiConfig = [];
+	public $aiEnabled = false;
+	public $aiHolder = null;
 	public $version;
 	public $playerMsgType = self::PLAYER_MSG_TYPE_MESSAGE;
 	public $playerLoginMsg = "";
@@ -749,6 +753,10 @@ class Server{
 	 */
 	public function getTick(){
 		return $this->tickCounter;
+	}
+	
+	public function getAIHolder(){
+		return $this->aiHolder;
 	}
 
 	/**
@@ -1563,6 +1571,21 @@ class Server{
 		$this->weatherRandomDurationMax = $this->getAdvancedProperty("level.weather-random-duration-max", 12000);
 		$this->lightningTime = $this->getAdvancedProperty("level.lightning-time", 200);
 		$this->lightningFire = $this->getAdvancedProperty("level.lightning-fire", false);
+		$this->aiEnabled = $this->getAdvancedProperty("ai.enable", false);
+ 		$this->aiConfig = [
+ 			"cow" => $this->getAdvancedProperty("ai.cow", true),
+ 			"chicken" => $this->getAdvancedProperty("ai.chicken", true),
+ 			"zombie" => $this->getAdvancedProperty("ai.zombie", 1),
+			"skeleton" => $this->getAdvancedProperty("ai.skeleton", true),
+ 			"pig" => $this->getAdvancedProperty("ai.pig", true),
+ 			"sheep" => $this->getAdvancedProperty("ai.sheep", true),
+ 			"creeper" => $this->getAdvancedProperty("ai.creeper", true),
+ 			"irongolem" => $this->getAdvancedProperty("ai.iron-golem", true),
+ 			"snowgolem" => $this->getAdvancedProperty("ai.snow-golem", true),
+ 			"pigzombie" => $this->getAdvancedProperty("ai.pigzombie", true),
+ 			"creeperexplode" => $this->getAdvancedProperty("ai.creeper-explode-destroy-block", false),
+ 			"mobgenerate" => $this->getAdvancedProperty("ai.mobgenerate", false),
+ 		];
 		$this->allowSnowGolem = $this->getAdvancedProperty("server.allow-snow-golem", false);
 		$this->allowIronGolem = $this->getAdvancedProperty("server.allow-iron-golem", false);
 		$this->autoClearInv = $this->getAdvancedProperty("player.auto-clear-inventory", true);
